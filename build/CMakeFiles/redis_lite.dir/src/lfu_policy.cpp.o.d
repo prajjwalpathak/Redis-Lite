ea1b53fea2acd35ee411c286fc044dd71deae104
prajjwalpathak <prajjwalpathak1105@gmail.com>
2026-01-03 23:20:32 +0530

    Add core engine, LRU eviction

--- build/CMakeFiles/redis_lite.dir/src/lfu_policy.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/redis_lite.dir/src/lfu_policy.cpp.o: \
- /mnt/d/Prajjwal/Code/Redis-Lite/src/lfu_policy.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/redis_lite.dir/src/lru_policy.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/redis_lite.dir/src/lru_policy.cpp.o: \
- /mnt/d/Prajjwal/Code/Redis-Lite/src/lru_policy.cpp \
- /usr/include/stdc-predef.h
